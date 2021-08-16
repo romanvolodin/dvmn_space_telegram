eed434ec90e7c73191d5d513235b9f505bf8cbf0
Roman Volodin <roman.volodin@gmail.com>
2021-08-16 17:12:25 +0300

    Add save image from SpaceX functions

--- main.py
+++ main.py
@@ -18,13 +18,29 @@ def save_image_from_url(url, save_path):
         image.write(response.content)
 
 
+def fetch_spacex_latest_launch_images():
+    SPACEX_API_URL = "https://api.spacexdata.com/v4/launches/latest"
+    response = requests.get(SPACEX_API_URL)
+    response.raise_for_status()
+    return response.json()["links"]["flickr"]["original"]
+
+
+def save_spacex_latest_launch_images(save_path):
+    saved_images = []
+    image_urls = fetch_spacex_latest_launch_images()
+    if not image_urls:
+        return
+    for index, image_url in enumerate(image_urls):
+        image_path = f"{save_path}/{index}.jpg"
+        save_image_from_url(image_url, image_path)
+        saved_images.append(image_path)
+    return saved_images
+
+
 if __name__ == "__main__":
     try:
         Path("images").mkdir(exist_ok=True)
     except PermissionError as err:
         exit(err)
 
-    try:
-        hubble_image = save_image_from_url(IMAGE_URL, "images/hubble.jpeg")
-    except requests.exceptions.HTTPError as err:
-        exit(err)
+    save_spacex_latest_launch_images("images")
